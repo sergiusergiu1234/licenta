9c36f61ab9bc77f61ce93017fa46692500e661b0
sergiusergiu1234 <sergiuc.stefan@gmail.com>
2023-02-22 20:58:25 +0200

    Update HomeController.java

--- Backend/Licenta/src/main/java/com/StefanSergiu/Licenta/controller/HomeController.java
+++ Backend/Licenta/src/main/java/com/StefanSergiu/Licenta/controller/HomeController.java
@@ -7,7 +7,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
-import javax.validation.Valid;
+
 
 @RestController
 public class HomeController {
