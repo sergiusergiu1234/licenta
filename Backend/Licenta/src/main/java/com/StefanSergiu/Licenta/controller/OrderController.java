e1b0a00b01475acb3cd86b36ac40e5671e4e1283
sergiusergiu1234 <sergiuc.stefan@gmail.com>
2023-02-27 12:24:33 +0200

    implemented JWT

---
+++ Backend/Licenta/src/main/java/com/StefanSergiu/Licenta/controller/OrderController.java
@@ -0,0 +1,35 @@
+package com.StefanSergiu.Licenta.controller;
+
+
+
+import com.StefanSergiu.Licenta.entity.Order;
+import com.StefanSergiu.Licenta.service.OrderService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@CrossOrigin
+@RequestMapping("/orders")
+public class OrderController {
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    public OrderController(OrderService orderService) {
+        this.orderService = orderService;
+    }
+
+    @PreAuthorize("hasAuthority('ROLE_USER')")
+    @PostMapping(path = "/user/orders/my")
+    public void postOrder(){}
+
+    @PreAuthorize("hasAuthority('ROLE_ADMIN')")
+    @GetMapping(path = "/admin/orders/all")
+    public List<Order> getAllOrders(){return orderService.getAllOrders(); }
+
+
+}
